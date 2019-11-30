145f490e8fc44978eaf5620a0d2341ac258447c6
gillesgaubert <gilles.gaubert@rocketmail.com>
2019-11-30 18:12:25 +0100

    add script and doc

---
+++ queryBuilder.py
@@ -0,0 +1,69 @@
+"""
+parser for input  query in dataloader
+input file : extract.txt
+output file(s) : out0.txt, out1.txt, ...
+2 arguments in command line : maxBuffer et idLength, par exemple :
+python queryBuilder.py 15000 15
+15000 et 15 are default if absents
+author : gilles gaubert
+"""
+
+import os    
+import sys
+    
+if (len(sys.argv)==3) :
+    MAXBUFFER=int(sys.argv[1])   #15000 max size buffer to query in dataloader
+    IDLENGTH=int(sys.argv[2])    #15 size of id in nb of characters
+else:
+    MAXBUFFER=15000
+    IDLENGTH=15
+
+os.chdir("./")
+myFile = open("extract.txt", "r")
+content = myFile.read()
+
+# treatment
+bufferSize=MAXBUFFER-MAXBUFFER%(IDLENGTH+3)    # to account for ' and ,
+print("Adjusted buffer size to account for separators = "+str(bufferSize))
+formatedContent=""
+for c in content:
+    if (c=="\n"):
+        formatedContent=formatedContent+"\',\'"
+    else:
+        formatedContent=formatedContent+c
+
+formatedContent="\'"+formatedContent+"\'"
+sizeFormatedContent=len(formatedContent)
+print("size formated content = "+str(sizeFormatedContent))
+cursor=0
+resizedContent=[]
+if (sizeFormatedContent<MAXBUFFER):
+    # the easy case
+    resizedContent.append(formatedContent)
+    print("Only one slice !")
+else:            
+    # more than one slice
+    residualSize=sizeFormatedContent
+    while (residualSize>bufferSize):
+        part=formatedContent[cursor:cursor+bufferSize]
+        cursor=cursor+bufferSize
+        residualSize=residualSize-bufferSize
+        resizedContent.append(part)
+    part=formatedContent[cursor:]
+    resizedContent.append(part)
+    for cont in resizedContent :
+        print("slice size = "+str(len(cont)))
+    print("number of slices = "+str(len(resizedContent)))
+        
+# saving output
+os.chdir("./outs/")
+for i in range (0,len(resizedContent)):
+    outputFileName="out"+str(i)+".txt"
+    print("creation : "+outputFileName)
+    
+    outputFile = open(outputFileName, "w")
+    outputFile.write(resizedContent[i])
+    outputFile.close()
+
+# closing input
+myFile.close()
---
+++ readme.txt
@@ -0,0 +1,16 @@
+readme : scriptRequete.py     auteur : gilles gaubert
+-------------------------
+
+Contexte : permettre de transformer un fichier contenant des id Salesforce issus d'une extraction avec un id par ligne en un fichier ou les id sont en une seule ligne de la forme:
+'id1','id2',...
+Avec des fichier de taille definissable par l'utilisateur (dataloader ne prends par exemple pas plus de 20000 caracteres dans les requetes).
+Dans le cas ou la quantite est superieurs les donnees seront decoupees en plusieurs fichiers de sortie.
+
+Prerequis : le script etant en python il faut avoir préalablement installé python 3 telechargeable sur python.org.
+
+Utilisation :
+fichier en entree : extract.txt
+fichier(s) en sortie : sortie0.txt, sortie1.txt, ... dans le dossier 'sortie'
+2 arguments a passer en ligne de commands : maxTampon et longueurId, par exemple à executer dans la ligne de commande :
+python scriptRequete.py 15000 15
+15000 et 15 sont prises par defaut si les arguments sont absents
